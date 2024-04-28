7b5a163cc28159e497c24a406e88e7818e120c08
ImTonyS <tony.villaverde@hotmail.com>
2024-04-28 12:41:16 -0600

    Minimalist Card finished

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    content: ["./index.html"],
+    theme: {
+      extend: {
+        fontFamily: {
+          'sora': ['Sora', 'sans-serif']
+        },
+        colors: {
+            moradoPerso: '#883AE1',
+            moraditoPerso: '#E6D6FC',
+        },
+      },
+    },
+    plugins: [],
+  }
